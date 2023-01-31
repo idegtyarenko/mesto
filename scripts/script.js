df9916e486f763b5503904877add288210aed794
Ivan Degtiarenko <degat@yandex.ru>
2023-01-31 00:08:47 +0000

    feat: Закрывать попап по клику на фон

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,18 @@ const popup = document.querySelector('.popup');
 const edit_button = document.querySelector('.profile__button_role_edit');
 const close_button = document.querySelector('.popup__close-button');
 
-const togglePopup = function() {
+const togglePopup = function(evt) {
   // Если попап не открыт
   // Заполнить поля значениями со страницы
 
-  popup.classList.toggle('popup_opened');
-  page.classList.toggle('page_popup-opened');
+  if (evt.target === this) {
+    popup.classList.toggle('popup_opened');
+    page.classList.toggle('page_popup-opened');
+  }
 };
 
 // Сохранение изменений
 
 edit_button.addEventListener('click', togglePopup);
 close_button.addEventListener('click', togglePopup);
-// Повесить закрытие на затемненный фон
+popup.addEventListener('click', togglePopup);
