ec24375bbddbfa93f145ee75230f3835175de785
Ivan Degtiarenko <degat@yandex.ru>
2023-01-31 00:21:21 +0000

    refactor: Переименовать переменные в camelCase

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 const page = document.querySelector('.page');
 const popup = document.querySelector('.popup');
-const edit_button = document.querySelector('.profile__button_role_edit');
-const close_button = document.querySelector('.popup__close-button');
+const editButton = document.querySelector('.profile__button_role_edit');
+const closeButton = document.querySelector('.popup__close-button');
 const name = document.querySelector('.profile__name');
 const title = document.querySelector('.profile__title');
-const name_field = document.querySelector('.popup__form-input_name_name');
-const title_field = document.querySelector('.popup__form-input_name_title');
+const nameField = document.querySelector('.popup__form-input_name_name');
+const titleField = document.querySelector('.popup__form-input_name_title');
 
 const togglePopup = function(evt) {
   if (evt.target === this) {
     if (!popup.classList.contains('popup_opened')) {
-      name_field.value = name.textContent;
-      title_field.value = title.textContent;
+      nameField.value = name.textContent;
+      titleField.value = title.textContent;
     }
     popup.classList.toggle('popup_opened');
     page.classList.toggle('page_popup-opened');
@@ -20,6 +20,6 @@ const togglePopup = function(evt) {
 
 // Сохранение изменений
 
-edit_button.addEventListener('click', togglePopup);
-close_button.addEventListener('click', togglePopup);
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
 popup.addEventListener('click', togglePopup);
