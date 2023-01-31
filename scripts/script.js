f554410eaf412e5e7a85d0a1f9068e3c981edaed
Ivan Degtiarenko <degat@yandex.ru>
2023-01-31 00:18:39 +0000

    feat: Предзаполнять поля формы

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,17 @@ const page = document.querySelector('.page');
 const popup = document.querySelector('.popup');
 const edit_button = document.querySelector('.profile__button_role_edit');
 const close_button = document.querySelector('.popup__close-button');
+const name = document.querySelector('.profile__name');
+const title = document.querySelector('.profile__title');
+const name_field = document.querySelector('.popup__form-input_name_name');
+const title_field = document.querySelector('.popup__form-input_name_title');
 
 const togglePopup = function(evt) {
-  // Если попап не открыт
-  // Заполнить поля значениями со страницы
-
   if (evt.target === this) {
+    if (!popup.classList.contains('popup_opened')) {
+      name_field.value = name.textContent;
+      title_field.value = title.textContent;
+    }
     popup.classList.toggle('popup_opened');
     page.classList.toggle('page_popup-opened');
   }
