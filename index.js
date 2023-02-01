c41e9517fff349285dd7b56683e8e2fdf853f2e8
Ivan Degtiarenko <degat@yandex.ru>
2023-02-01 00:03:37 +0000

    refactor: Вынести форму в отдельный блок

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const editButton = document.querySelector('.profile__button_role_edit');
 const closeButton = document.querySelector('.popup__close-button');
 const name = document.querySelector('.profile__name');
 const title = document.querySelector('.profile__title');
-const form = document.querySelector('.popup__form');
-const nameField = document.querySelector('.popup__form-input_name_name');
-const titleField = document.querySelector('.popup__form-input_name_title');
+const form = document.querySelector('.form');
+const nameField = document.querySelector('.form__input_name_name');
+const titleField = document.querySelector('.form__input_name_title');
 
 const togglePopup = function(evt) {
   if (evt.target === this || evt.type === 'submit') {
