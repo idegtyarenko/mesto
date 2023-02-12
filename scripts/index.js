2dd1cb48dfbeb9effb2938235da2913355c5c0ac
Ivan Degtiarenko <degat@yandex.ru>
2023-02-12 12:46:40 +0000

    feat: Брать места из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,49 @@
-const page = document.querySelector('.page');
-const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__button_role_edit');
-const closeButton = document.querySelector('.popup__close-button');
-const name = document.querySelector('.profile__name');
-const title = document.querySelector('.profile__title');
-const form = document.querySelector('.form');
-const nameField = document.querySelector('.form__input_name_name');
-const titleField = document.querySelector('.form__input_name_title');
+// Инициализируем карточки
 
-const initForm = function (evt) {
-  nameField.value = name.textContent;
-  titleField.value = title.textContent;
-  togglePopup(evt);
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const initPlaces = function (cards) {
+  const template = document.querySelector('#place-template');
+  for (const card of cards) {
+    const placeNode = template.content.cloneNode(true);
+    placeNode.querySelector('.places__place-photo').src = card.link;
+    placeNode.querySelector('.places__place-name').textContent = card.name;
+    document.querySelector('.places').append(placeNode);
+  }
 };
+
+initPlaces(initialCards);
+
+
+// Попап
+
+const popup = document.querySelector('.popup');
+const page = document.querySelector('.page');
 
 const togglePopup = function (evt) {
   if (
@@ -26,6 +57,28 @@ const togglePopup = function (evt) {
   }
 };
 
+const closeButton = document.querySelector('.popup__close-button');
+closeButton.addEventListener('click', togglePopup);
+popup.addEventListener('click', togglePopup);
+
+
+// Форма
+
+const name = document.querySelector('.profile__name');
+const title = document.querySelector('.profile__title');
+const form = document.querySelector('.form');
+const nameField = document.querySelector('.form__input_name_name');
+const titleField = document.querySelector('.form__input_name_title');
+
+const initForm = function (evt) {
+  nameField.value = name.textContent;
+  titleField.value = title.textContent;
+  togglePopup(evt);
+};
+
+const editButton = document.querySelector('.profile__button_role_edit');
+editButton.addEventListener('click', initForm);
+
 const handleFormSubmit = function (evt) {
   evt.preventDefault();
   name.textContent = nameField.value;
@@ -33,7 +86,4 @@ const handleFormSubmit = function (evt) {
   togglePopup(evt);
 }
 
-editButton.addEventListener('click', initForm);
-closeButton.addEventListener('click', togglePopup);
-popup.addEventListener('click', togglePopup);
 form.addEventListener('submit', handleFormSubmit);
