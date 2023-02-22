73d41c7bce206979447378c28733c745f9da4e56
Ivan Degtiarenko <degat@yandex.ru>
2023-02-22 23:08:53 +0000

    Сверстать ошибки валидации

--- scripts/index.js
+++ scripts/index.js
@@ -178,3 +178,15 @@ function submitAddPlaceForm (evt) {
 }
 
 addPlacePopup.addEventListener('submit', submitAddPlaceForm);
+
+
+// Валидация форм
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
