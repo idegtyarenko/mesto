be33b247b9761ba4f75b25b246afe70b2bbdf3eb
Ivan Degtiarenko <degat@yandex.ru>
2023-02-23 15:33:35 +0000

    feat: Добавить валидацию в формах

--- scripts/index.js
+++ scripts/index.js
@@ -27,6 +27,14 @@ const initialPlaces = [
   }
 ];
 
+const validationParams = {
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-button',
+  inactiveButtonClass: 'button_state_disabled',
+  inputErrorClass: 'form__input_state_invalid',
+  errorIdPostfix: '-error'
+};
+
 const page = document.querySelector('.page');
 const profileName = page.querySelector('.profile__name');
 const profileTitle = page.querySelector('.profile__title');
@@ -192,21 +200,62 @@ addFormOpenListeners();
 
 // Валидация и применение форм
 
-function enableValidation (params) {
-  for (const formElement of document.forms) {
-    const formDescription = forms[formElement.name];
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-      formDescription.submitFunction();
-      closePopup(evt);
+function showInputError (formElement, inputElement, errorMessage, validationParams) {
+  const errorElementSelector = '#' + inputElement.name + validationParams.errorIdPostfix;
+  const errorElement = formElement.querySelector(errorElementSelector);
+  inputElement.classList.add(validationParams.inputErrorClass);
+  errorElement.textContent = errorMessage;
+}
+
+function hideInputError (formElement, inputElement, validationParams) {
+  const errorElement = formElement.querySelector('#' + inputElement.name + validationParams.errorIdPostfix);
+  inputElement.classList.remove(validationParams.inputErrorClass);
+  errorElement.textContent = '';
+}
+
+function checkInputValidity (formElement, inputElement, validationParams) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, validationParams);
+  } else {
+    hideInputError(formElement, inputElement, validationParams);
+  }
+}
+
+function hasInvalidInput (inputList) {
+  return inputList.some(input => {
+    return !input.validity.valid;
+  });
+}
+
+function toggleButtonState (inputList, buttonElement, validationParams) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(validationParams.inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(validationParams.inactiveButtonClass);
+  }
+}
+
+function setValidationListeners (formElement, validationParams) {
+  const inputList = Array.from(formElement.querySelectorAll(validationParams.inputSelector));
+  const buttonElement = formElement.querySelector(validationParams.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, validationParams);
+  for (const inputElement of inputList) {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, validationParams);
+      toggleButtonState(inputList, buttonElement, validationParams);
     })
   }
 }
 
-enableValidation({
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__submit-button',
-  inactiveButtonClass: 'button_state_disabled',
-  inputErrorClass: 'form__input_state_invalid',
-  errorIdPostfix: '-error'
-});
+function enableValidation (validationParams) {
+  for (const formElement of document.forms) {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      forms[formElement.name].submitFunction();
+      closePopup(evt);
+    })
+    setValidationListeners(formElement, validationParams);
+  }
+}
+
+enableValidation(validationParams);
